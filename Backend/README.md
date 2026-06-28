6dd8002a8a9e302743910d0614de1b8187140991
Anderson Anan <andersoncaporale@outlook.com>
2026-06-28 00:54:05 -0300

    feat: add simple RAG

--- Backend/README.md
+++ Backend/README.md
@@ -1,21 +1,24 @@
-Criar o ambiente venv:
+## Criar o ambiente venv:
 python -m venv venv     
 
 
-Habilitar o ambiente venv:
+##  Habilitar o ambiente venv:
 venv\Scripts\activate    
 
 
-Instalar as dependências (pip install):
-langchain_groq
-dotenv
-fastapi
-uvicorn
+##  Instalar as dependências (pip install):
+pip install -r requirements.txt
 
 
-Criar arquivo .env com as API_KEYS:
+##  Criar arquivo .env com as API_KEYS:
 GROQ_API_KEY="sua_key"
 
+GOOGLE_API_KEY=""
 
-Rodar o arquivo server.py (trocar o PATH para o caminho que o projeto está):
+QDRANT_API_KEY=""
+
+QDRANT_URL=""
+
+
+##  Rodar o arquivo server.py (trocar o PATH para o caminho que o projeto está):
 c:\PATH\Projeto\Backend\venv\Scripts\python.exe c:/PATH/Projeto/Backend/server.py
--- Backend/chain_atendimento.py
+++ Backend/chain_atendimento.py
@@ -4,10 +4,19 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel
 from operator import itemgetter
+from rag_Qdrant import conecta_banco_vetorial_pre_criado
 
 # Carregar as chaves APIs presentes no arquivo .env
 load_dotenv()
 # --------------------------------------------------------------------------------
+
+# Conecta Banco Vetorial
+db_vetorial = conecta_banco_vetorial_pre_criado()
+db_retriever = db_vetorial.as_retriever(search_kwargs={'k': 5})
+
+"""Função para pegar o conteúdo de cada chunk e criar um unico texto/string"""
+def cria_texto_dos_documentos_retornados(documentos):
+    return "\n\n".join(doc.page_content for doc in documentos)
 
 # Instanciar um chatmodel para comunicarmos com os modelos LLMs
 modelo_groq = ChatGroq(model="openai/gpt-oss-20b", temperature=0)
@@ -22,19 +31,29 @@ oficial da instituição.
 2 - Sempre se baseie no contexto que é entregue entre as tags <contexto></contexto>. As informações presentes nestas tags
 foram obtidas de uma base de conhecimento.
 3 - Evite falar 'no contexto...' ou 'conforme o contexto...' porque o usuário desconheçe sobre a presença desse contexto.
+4 - Escreva o texto em português simples. Não utilize sequências Unicode escapadas (como \u202f, \u00a0 ou \n).
+5 - Use apenas caracteres Unicode normais e espaços comuns. Retorne o texto já renderizado, sem códigos de escape.
+
 
 ## Contexto Recuperado:
 <contexto>
+{contexto_obtido}
 </contexto>
 """
+
+def remover_unicode(texto):
+    return texto.replace("\u202f", " ")
 
 # Criando o ChatPromptTemplate com a entrada do usuário e o histórico:
 atendimento_prompt_template = ChatPromptTemplate([("system", sys_atendimento_geral_prompt),
                                                   ("human", "Mensagem do usuário: {input_user}"),
                                                   ])
 
-chain_de_atendimento_geral = (RunnableParallel({"input_user": itemgetter("input_user")
+chain_de_atendimento_geral = (RunnableParallel({"input_user": itemgetter("input_user"),
+                                                "contexto_obtido": itemgetter("input_user") | db_retriever | cria_texto_dos_documentos_retornados
+                                       
                                 })
                                 | atendimento_prompt_template
                                 | modelo_groq
-                                | StrOutputParser())
+                                | StrOutputParser()
+                                | remover_unicode)
